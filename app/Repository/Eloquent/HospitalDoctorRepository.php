7c7c8846a4ea5a4eda8c8c9d2202d7a242d2e86c
Madhu-440 <madhu.rachuri@gssinfotech.com>
2021-09-15 11:07:01 +0530

    docrepo

--- app/Repository/Eloquent/HospitalDoctorRepository.php
+++ app/Repository/Eloquent/HospitalDoctorRepository.php
@@ -47,8 +47,8 @@ class HospitalDoctorRepository
             $getDoctors = HospitalDoctor::where('division_id',"=", $division['id'])->count();
             $output[] = array(
                 $division['division_name'],
-                $getDoctors, $getDoctors,
-
+                $getDoctors,
+                $getDoctors,
                 $colors[$rand]
 
 
@@ -86,6 +86,57 @@ class HospitalDoctorRepository
 
 
     }
+    /*public function getHospitalDoctorData()
+
+    {
+        $colors=array('#ff0000','#ffc0cb','#2e8b57','#6a5acd','#008080','#ffff00','#9acd32','#6a5acd','#008080','#2e8b57');
+
+
+        $getDivision= HospitalDivision::select('id','division_name')->get();
+        foreach($getDivision as $division){
+            $rand= rand(0,9);
+            $getDoctors = HospitalDoctor::where('division_id',"=", $division['id'])->count();
+            $output[] = array(
+                $division['division_name'],
+                $getDoctors,
+                $getDoctors,
+                $colors[$rand]
+
+
+
+               );
+
+           // echo  "<pre>"; print_r($division['id']); exit;
+
+        }
+        return  $data =json_encode($output);
+      //  echo  "<pre>"; print_r($getDivision); exit;
+
+        // for ($i=0; $i<=4; $i++){
+        //     //$divisions=array('cardiology','neurology','oncology','prdiatrics','surgery');
+
+        //     $divisionnames=HospitalDivision::pluck('division_name');
+        //     //$doctorscount=HospitalDoctor::where('doctor_name','=','$divisionnames')->count();
+        //     //$doctorscount=HospitalDivision::join('hospital_doctors','hospital_divisions.id', "=", 'hospital_doctors.division_id')->select('hospital_doctors.doctor_name','hospital_divisions.division_name')->count();
+        //     $doctorscount=HospitalDivision::join('hospital_doctors','hospital_doctors.division_id',"=",'hospital_divisions.id')
+        //     ->where('hospital_doctors.doctor_name',"=",'hospital_divisions.division_name')->count();
+        //     $output[] = array(
+        //         $divisionnames,
+        //         $doctorscount,
+        //         $doctorscount,
+        //         $colors[$i]
+        //     );
+            //return  $data =json_encode($output);
+
+
+
+       // }
+            //echo "<pre>";print_r($dates);
+
+
+
+
+    }*/
     public function gdf()
 
     {
@@ -94,7 +145,9 @@ class HospitalDoctorRepository
         for ($i=0; $i<=4; $i++){
             //$divisions=array('cardiology','neurology','oncology','prdiatrics','surgery');
 
-            $divisionnames=HospitalDivision::pluck('division_name');
+            $divisionnames=HospitalDivision::join('hospital_patient_details','hospital_divisions.id',"=",'hospital_patient_details.division_id')
+            ->where('hospital_divisions.id',"=",'hospital_patient_details.division_id')->count();
+            //->where('hospital_doctors.id',"=",'hospital_patient_details')->count();
             //$doctorscount=HospitalDoctor::where('doctor_name','=','$divisionnames')->count();
             //$doctorscount=HospitalDivision::join('hospital_doctors','hospital_divisions.id', "=", 'hospital_doctors.division_id')->select('hospital_doctors.doctor_name','hospital_divisions.division_name')->count();
             //$doctorscount=HospitalDivision::join('hospital_doctors','hospital_doctors.division_id',"=",'hospital_divisions.id')
